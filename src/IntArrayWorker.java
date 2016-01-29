9ca6c62f8be37b1dceb28df60a55e5c2c4781cf2
Bilal Reza <rezasb@s.dcsdk12.org>
2016-01-29 10:22:15 -0700

    Done

--- src/IntArrayWorker.java
+++ src/IntArrayWorker.java
@@ -110,4 +110,14 @@ public class IntArrayWorker {
 		return max;
 	}
 
+	public void getColTotal() {
+		int total = 0;
+		for (int row = 0; row < matrix.length; row++) {
+			for (int col = 0; col < matrix[0].length; col++) {
+				total = total + matrix[col];
+			}
+		}
+		return total;
+	}
+
 }
